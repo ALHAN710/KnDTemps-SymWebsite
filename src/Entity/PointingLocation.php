41e1b7a2051cd7066ccdca0827a8711996ea7b3b
PASCAL ALHADOUM <alhadoumpascal@gmail.com>
2020-12-01 11:40:51 +0100

    Gestion des contraintes de validation du formulaire des Zones de Pointage

--- src/Entity/PointingLocation.php
+++ src/Entity/PointingLocation.php
@@ -2,10 +2,13 @@
 
 namespace App\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\Collection;
 use App\Repository\PointingLocationRepository;
 use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
-use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Constraints\PositiveOrZero;
 
 /**
  * @ORM\Entity(repositoryClass=PointingLocationRepository::class)
@@ -31,6 +34,7 @@ class PointingLocation
 
     /**
      * @ORM\Column(type="float")
+     * @Assert\PositiveOrZero
      */
     private $ray;
 
@@ -47,6 +51,7 @@ class PointingLocation
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Veuillez Entrer le nom du lieu de Pointage")
      */
     private $name;
 
--- src/Form/PointingLocationType.php
+++ src/Form/PointingLocationType.php
@@ -18,22 +18,26 @@ class PointingLocationType extends ApplicationType
             ->add(
                 'latitude',
                 NumberType::class,
-                $this->getConfiguration("Latitude", "Coordonnée en Latitude...")
+                $this->getConfiguration("Latitude (*)", "Coordonnée en Latitude (Obligatoire)...")
             )
             ->add(
                 'longitude',
                 NumberType::class,
-                $this->getConfiguration("Longitude", "Coordonnée en Longitude...")
+                $this->getConfiguration("Longitude (*)", "Coordonnée en Longitude (Obligatoire)...")
             )
             ->add(
                 'ray',
                 NumberType::class,
-                $this->getConfiguration("Rayon (km)", "Rayon de pointage en km svp...")
+                $this->getConfiguration("Rayon (km)(*)", "Rayon de pointage en km svp (Obligatoire)...", [
+                    'attr' => [
+                        'min' => 0
+                    ]
+                ])
             )
             ->add(
                 'name',
                 TextType::class,
-                $this->getConfiguration("Nom", "Nom du Lieu svp...")
+                $this->getConfiguration("Nom (*)", "Nom du Lieu svp (Obligatoire)...")
             )
             //->add('enterprise')
         ;
