94c08330464e7baaa730651f9a01e829c173ac43
PASCAL ALHADOUM <alhadoumpascal@gmail.com>
2021-01-10 04:32:40 +0100

    Contrôle d'accès pour les pointages

--- src/Controller/PointingsController.php
+++ src/Controller/PointingsController.php
@@ -113,7 +113,7 @@ class PointingsController extends ApplicationController
      * 
      * @Route("/pointing/action", name="pointing_action")
      * 
-     * @Security( "is_granted('ROLE_USER')" )
+     * @Security( "is_granted('ROLE_USER') " )
      *
      * @param Request $request
      * @param EntityManagerInterface $manager
@@ -121,78 +121,86 @@ class PointingsController extends ApplicationController
      */
     public function pointingAction(Request $request, EntityManagerInterface $manager)
     {
-        $paramJSON = $this->getJSONRequest($request->getContent());
-        $ray  = 0;
-        $lat  = 0;
-        $long = 0;
 
-        if ((array_key_exists("type", $paramJSON) && !empty($paramJSON['type'])) && (array_key_exists("lat", $paramJSON) && !empty($paramJSON['lat'])) && (array_key_exists("long", $paramJSON) && !empty($paramJSON['long']))) {
-            if (count($this->getUser()->getEnterprise()->getPointingLocations()) > 0) {
-                $ray  = $this->getUser()->getTeam()->getPointingLocation()->getRay();
-                $lat  = $this->getUser()->getTeam()->getPointingLocation()->getLatitude();
-                $long = $this->getUser()->getTeam()->getPointingLocation()->getLongitude();
-            } else {
-                return $this->json([
-                    'code'    => 403,
-                    'message' => 'Zone de pointage non définie !',
-                ], 200);
-            }
-            $distance = $this->calcCrow($lat, $long, $paramJSON['lat'], $paramJSON['long']);
-            if ($distance <= $ray) {
-                $date = new DateTime(date('Y-m-d H:i:s'));
-                if ($paramJSON['type'] === 'In') {
-                    $pointing = new Pointing();
-                    //$status = $this->getUser()->getRoles()[0] === 'ROLE_USER' ? 'on pending' : 'approved';
-                    $status = 'on pending';
-                    $pointing->setEmployee($this->getUser())
-                        ->setStatut($status)
-                        ->setTimeIn($date);
+        if ($this->getUser()->getEnterprise()->getIsActivated()) {
+            $paramJSON = $this->getJSONRequest($request->getContent());
+            $ray  = 0;
+            $lat  = 0;
+            $long = 0;
 
-                    $manager->persist($pointing);
-                } else if ($paramJSON['type'] === 'Out') {
-                    $point = $manager->createQuery("SELECT p
-                                                    FROM App\Entity\Pointing p
-                                                    WHERE p.employee = :empId
-                                                    AND p.timeIn LIKE :dat 
-                                                         
-                    ")
-                        ->setParameters([
-                            'empId' => $this->getUser()->getId(),
-                            'dat'   => $date->format('Y-m-d') . '%'
-                        ])
-                        ->getResult();
-                    //dump($point);
-                    if (!empty($point)) {
-                        $pointing = $point[0];
-                        $pointing->setTimeOut($date);
+            if ((array_key_exists("type", $paramJSON) && !empty($paramJSON['type'])) && (array_key_exists("lat", $paramJSON) && !empty($paramJSON['lat'])) && (array_key_exists("long", $paramJSON) && !empty($paramJSON['long']))) {
+                if (count($this->getUser()->getEnterprise()->getPointingLocations()) > 0) {
+                    $ray  = $this->getUser()->getTeam()->getPointingLocation()->getRay();
+                    $lat  = $this->getUser()->getTeam()->getPointingLocation()->getLatitude();
+                    $long = $this->getUser()->getTeam()->getPointingLocation()->getLongitude();
+                } else {
+                    return $this->json([
+                        'code'    => 403,
+                        'message' => 'Zone de pointage non définie !',
+                    ], 200);
+                }
+                $distance = $this->calcCrow($lat, $long, $paramJSON['lat'], $paramJSON['long']);
+                if ($distance <= $ray) {
+                    $date = new DateTime(date('Y-m-d H:i:s'));
+                    if ($paramJSON['type'] === 'In') {
+                        $pointing = new Pointing();
+                        //$status = $this->getUser()->getRoles()[0] === 'ROLE_USER' ? 'on pending' : 'approved';
+                        $status = 'on pending';
+                        $pointing->setEmployee($this->getUser())
+                            ->setStatut($status)
+                            ->setTimeIn($date);
 
                         $manager->persist($pointing);
-                    } else {
-                        return $this->json([
-                            'code'    => 403,
-                            'message' => "Pointage d'entré non effectué !",
-                        ], 200);
-                    }
-                    //$pointing->setTimeOut($date);
-                }
-                $manager->flush();
+                    } else if ($paramJSON['type'] === 'Out') {
+                        $point = $manager->createQuery("SELECT p
+                                                        FROM App\Entity\Pointing p
+                                                        WHERE p.employee = :empId
+                                                        AND p.timeIn LIKE :dat 
+                                                             
+                        ")
+                            ->setParameters([
+                                'empId' => $this->getUser()->getId(),
+                                'dat'   => $date->format('Y-m-d') . '%'
+                            ])
+                            ->getResult();
+                        //dump($point);
+                        if (!empty($point)) {
+                            $pointing = $point[0];
+                            $pointing->setTimeOut($date);
 
+                            $manager->persist($pointing);
+                        } else {
+                            return $this->json([
+                                'code'    => 403,
+                                'message' => "Pointage d'entré non effectué !",
+                            ], 200);
+                        }
+                        //$pointing->setTimeOut($date);
+                    }
+                    $manager->flush();
+
+                    return $this->json([
+                        'code'     => 200,
+                        'distance' => $distance . ' km',
+                        //'point'    => $point
+                    ], 200);
+                }
                 return $this->json([
-                    'code'     => 200,
-                    'distance' => $distance . ' km',
-                    //'point'    => $point
+                    'code'    => 403,
+                    'message' => 'Zone de pointage non autorisée !', 'distance' => $distance . ' km',
                 ], 200);
             }
+
+            return $this->json([
+                'code' => 403,
+                'message' => 'Empty Array or Not exists !',
+            ], 403);
+        } else {
             return $this->json([
                 'code'    => 403,
-                'message' => 'Zone de pointage non autorisée !', 'distance' => $distance . ' km',
+                'message' => 'Abonnement Expiré ou en attente de paiement pour validation',
             ], 200);
         }
-
-        return $this->json([
-            'code' => 403,
-            'message' => 'Empty Array or Not exists !',
-        ], 403);
     }
 
     /**
